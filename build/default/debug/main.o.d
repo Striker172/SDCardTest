aec0cf74f4ec02565cd0e90d0f642ff930c5b349
Ernest Blake <59033052+Striker172@users.noreply.github.com>
2025-04-24 15:08:46 -0500

    Add files via upload

---
+++ build/default/debug/main.o.d
@@ -0,0 +1,16 @@
+build/default/debug/main.o: main.c mcc_generated_files/system.h \
+ mcc_generated_files/fatfs/fatfs_demo.h mcc_generated_files/fatfs/ff.h \
+ mcc_generated_files/fatfs/integer.h mcc_generated_files/fatfs/ffconf.h \
+ mcc_generated_files/sd_spi/sd_spi.h
+
+mcc_generated_files/system.h:
+
+mcc_generated_files/fatfs/fatfs_demo.h:
+
+mcc_generated_files/fatfs/ff.h:
+
+mcc_generated_files/fatfs/integer.h:
+
+mcc_generated_files/fatfs/ffconf.h:
+
+mcc_generated_files/sd_spi/sd_spi.h:
---
+++ build/default/production/main.o.d
@@ -0,0 +1,13 @@
+build/default/production/main.o: main.c mcc_generated_files/system.h \
+ mcc_generated_files/fatfs/fatfs_demo.h mcc_generated_files/fatfs/ff.h \
+ mcc_generated_files/fatfs/integer.h mcc_generated_files/fatfs/ffconf.h
+
+mcc_generated_files/system.h:
+
+mcc_generated_files/fatfs/fatfs_demo.h:
+
+mcc_generated_files/fatfs/ff.h:
+
+mcc_generated_files/fatfs/integer.h:
+
+mcc_generated_files/fatfs/ffconf.h:
